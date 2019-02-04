66558c708668b01194506c51ce2d2212899d182a
michaeljymsgutierrrez <leeez711@gmail.com>
2019-02-04 22:51:53 +0800

    Updated component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  componentDidMount = () => {
+    this.rollCamera();
+  };
+  rollCamera = () => {
+    /**
+     *  Roll Camera Function
+     */
+    let video = document.getElementById("video");
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({ video: true }).then(
+        stream => {
+          video.srcObject = stream;
+          video.play();
+        },
+        error => {
+          console.log(error);
+        }
+      );
+    }
+  };
   render() {
-    return <div>Chael</div>;
+    return (
+      <div className="photo">
+        <video id="video" width="568" height="450" />
+        <canvas id="canvas" width="568" height="426" />
+      </div>
+    );
   }
 }
 
