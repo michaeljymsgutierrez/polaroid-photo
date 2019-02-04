0695169471c47f9b293c37194d76b8eac4391b6e
michaeljymsgutierrrez <leeez711@gmail.com>
2019-02-04 23:04:53 +0800

    Updated app component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: ""
+    };
+  }
   componentDidMount = () => {
+    /**
+     * Initialize Data
+     */
     this.rollCamera();
   };
   rollCamera = () => {
@@ -12,11 +21,12 @@ class App extends Component {
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
       navigator.mediaDevices.getUserMedia({ video: true }).then(
         stream => {
+          this.setState({ message: JSON.stringify(stream) });
           video.srcObject = stream;
           video.play();
         },
         error => {
-          console.log(error);
+          this.setState({ message: JSON.stringify(error) });
         }
       );
     }
@@ -24,6 +34,7 @@ class App extends Component {
   render() {
     return (
       <div className="photo">
+        {this.state.message}
         <video id="video" width="568" height="450" />
         <canvas id="canvas" width="568" height="426" />
       </div>
